788b7d9dbcae85af8f2544dc5bff891b405fcbe8
Azhar Ali <AzharRafi17>
2025-01-09 11:45:04 +0500

    icon update

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,6 @@ import { RouterView } from 'vue-router';
 </script>
 
 <template>
-  <head>
-    <!-- Add this inside your <head> tag in index.html or globally -->
-<link href="https://fonts.googleapis.com/css2?family=Audiowide&display=swap" rel="stylesheet">
-
-  </head>
   <Navbar />
   <RouterView />
 </template>
